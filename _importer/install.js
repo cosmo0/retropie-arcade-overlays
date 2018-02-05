0e8e6109125aabc5714f15029b8479cf9250eb1f
Thomas Broust <thomas.broust@gmail.com>
2018-02-05 21:58:59 +0100

    Future install script

---
+++ _importer/install.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var path = require('path');
+var readlineSync = require('readline-sync');
+
+// ask the user which overlay package they want
+
+// ask the path to the roms
+
+// ask the path to retropie config folder
+
+// ask whether to overwrite files if any
+
+// list the available rom configs
+
+// copy the rom and overlay configs if the matching rom is found
